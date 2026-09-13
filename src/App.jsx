3402d14694b3b000d62662e00b8a1c558be0bf0e
yashinn007 <yashinn2024@gmail.com>
2026-09-13 15:58:58 +0600

    innitialize & created header

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <header>
+        <Navbar></Navbar>
+      </header>
+      <h1>Hi, i am Shihab</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import DigiTools from "../../assets/DigiTools.png";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100 shadow-sm container mx-auto">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              aria-label="Menu"
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex={-1}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <a>Products</a>
+            </li>
+            <li>
+              <a>Features</a>
+            </li>
+            <li>
+              <a>Pricing</a>
+            </li>
+            <li>
+              <a>Testimonials</a>
+            </li>
+            <li>
+              <a>FAQ</a>
+            </li>
+          </ul>
+        </div>
+        <a href="">
+          <img className="w-35 md:w-45" src={DigiTools} alt="" />
+        </a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 font-semibold text-[16px]">
+          <li>
+            <a>Products</a>
+          </li>
+          <li>
+            <a>Features</a>
+          </li>
+          <li>
+            <a>Pricing</a>
+          </li>
+          <li>
+            <a>Testimonials</a>
+          </li>
+          <li>
+            <a>FAQ</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end gap-5">
+        {/* ---------------card icon------------------ */}
+        <div className="indicator">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            {" "}
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="1.5"
+              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+            />{" "}
+          </svg>
+          <span className="badge badge-sm indicator-item bg-red-500 text-white">
+            8
+          </span>
+        </div>
+        {/* -------------------------------- */}
+        <a href="#" className="font-semibold text-[16px]">
+          Login
+        </a>
+        <a className="hidden md:block pt-1.5 btn bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-4xl text-white font-semibold text-[16px]">
+          Get Started
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
